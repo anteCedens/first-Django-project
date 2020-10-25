5a506ef9405544361912b7c52c2eadb0027f3ef7
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-25 20:34:53 +0000

    Correcting typo in forms.py

--- todo/forms.py
+++ todo/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Item
 
 
-class ItemForm(Forms.ModelForm):
+class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['name', 'done']
